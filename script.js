6553059792396658de2af84cf1a18d1ebeaf724a
Aimee Gibson <aimjo617@gmail.com>
2023-06-29 08:31:07 -0600

    added comments

--- script.js
+++ script.js
@@ -4,15 +4,20 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
+	//converts string to lower case
 	const val = str.toLowerCase(); 
+	//filters over fruit array to find any matches from the string
 	const results = fruit.filter(item => item.toLowerCase().includes(val))
 	return results;
 }
 
 function searchHandler(e) {
+	//retrieves the current value from the search input
 	const inputVal = e.currentTarget.value;
 	let results = [];
+	//checks to see if the imput field is empty or not
 	if (inputVal.length > 0) {
+		//adds search to empty results array
 		results = search(inputVal);
 	}
 	showSuggestions(results, inputVal);
@@ -25,12 +30,15 @@ function showSuggestions(results, inputVal) {
 	if (results.length > 0) {
 		for (i = 0; i < results.length; i++) {
 			let item = results[i];
+			// Highlights only the first match
+			// got the RegExp from chatgpt looking for a way to match with bold
 			const match = item.match(new RegExp(inputVal, 'i'));
 			item = item.replace(match[0], `<strong>${match[0]}</strong>`);
 			suggestions.innerHTML += `<li>${item}</li>`;
 		}
 		suggestions.classList.add('has-suggestions');
 	} else {
+		//removes suggestion when it no longer matches or input is deleted
 		results = [];
 		suggestions.innerHTML = '';
 		suggestions.classList.remove('has-suggestions');
@@ -38,6 +46,7 @@ function showSuggestions(results, inputVal) {
 }
 
 function useSuggestion(e) {
+	//adds suggestion to the input field after uses selects suggestion and then clears the suggestion list
 	input.value = e.target.innerText; 
 	input.focus();
 	suggestions.innerHTML = '';
